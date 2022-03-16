80e9aefa8e435d9a841d65049beb41943d6b2a8f
Capatha <tenshipixel@gmail.com>
2022-03-16 15:01:34 +0000

    Move :D

--- Light Jumper Project/Assets/Scripts/PlayerMovement.cs
+++ Light Jumper Project/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 3.0f;
+
     private Rigidbody2D physicsBody = null;
 
 
@@ -13,12 +15,18 @@ public class PlayerMovement : MonoBehaviour
     {
         physicsBody = GetComponent<Rigidbody2D>();
 
-        physicsBody.velocity = new Vector2(2, 0);
+        /* physicsBody.velocity = new Vector2(2, 0); */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Ask Unity's input manager for current Horizontal / Vertical axis value. Between -1 and 1.
+        float horiVal = Input.GetAxis("Horizontal");
+        float vertVal = Input.GetAxis("Vertical");
+
+        Vector2 newVal = new Vector2 (horiVal, vertVal);
+        newVal = newVal * moveSpeed;
+        physicsBody.velocity = newVal;
     }
 }
