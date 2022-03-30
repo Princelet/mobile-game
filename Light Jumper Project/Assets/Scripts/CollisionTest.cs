b10cc7be5f5d574e67cef83a1cd65b5ab537d4db
Capatha <tenshipixel@gmail.com>
2022-03-30 15:54:25 +0100

    Physics :D

---
+++ Light Jumper Project/Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // get thing we ran into
+        GameObject otherObject = collision.gameObject;
+
+        // get thing name
+        string otherName = otherObject.name;
+
+        // get thing tag
+        string otherTag = otherObject.tag;
+
+        // get layer (physics) of the thing
+        int otherLayer = otherObject.layer;
+
+        // check if script is attached and get
+        // getting a component from other object
+        SpriteRenderer spriteRenderer = otherObject.GetComponent<SpriteRenderer>();
+
+        Debug.Log("Name: " + otherName);
+        Debug.Log("Name: " + otherTag);
+        Debug.Log("Name: " + otherLayer);
+        if (spriteRenderer != null)
+            spriteRenderer.color = Color.white;
+    }
+}
